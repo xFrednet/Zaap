d21cd5b71cc4f32668fa539394771f6e5639472c
xFrednet <xFrednet@gmail.com>
2016-12-12 22:38:53 +0100

    I forgot the main files

--- Zaap/src/graphics/Renderer3D.cpp
+++ Zaap/src/graphics/Renderer3D.cpp
@@ -1,41 +1,117 @@
 ﻿#include "Renderer3D.h"
+#include <app/Window.h>
 
-zaap::graphics::Renderer3D* zaap::graphics::Renderer3D::Init()
-{
-}
+#include "Scene.h"
 
-zaap::graphics::Renderer3D::Renderer3D()
-{
-}
+namespace zaap { namespace graphics {
 
-void zaap::graphics::Renderer3D::loadTransformationMatrix(const Mat4& transformationMatrix) const
-{
-}
+	Renderer3D* Renderer3D::Init()
+	{
+		//TODO create a D3D instance
+	}
 
-void zaap::graphics::Renderer3D::loadLightSetup(const LightSetup& LightSetup) const
-{
-}
+	Renderer3D::Renderer3D()
+		: m_ActiveShaderType(ZA_SHADER_UNKNOWN)
+	{
+		m_Width = Window::GetWidth();
+		m_Height = Window::GetWidth();
+	}
 
-void zaap::graphics::Renderer3D::loadScene(const Scene* scene) const
-{
-}
+	void Renderer3D::loadTransformationMatrix(const Mat4& transformationMatrix) const
+	{
+		switch (m_ActiveShaderType)
+		{
+		case ZA_SHADER_TEXTURE_SHADER:
+			if (m_TextureShader)
+				m_TextureShader->setTransformationMatrix(transformationMatrix);
+			return;
+		case ZA_SHADER_MATERIAL_SHADER:
+			if (m_MaterialShader)
+				m_MaterialShader->setTransformationMatrix(transformationMatrix);
+			return;
+		case ZA_SHADER_TERRAIN_SHADER:
+			if (m_TerrainShader)
+				m_TerrainShader->setTransformationMatrix(transformationMatrix);
+			return;
+		case ZA_SHADER_FONT_SHADER_2D:
+		case ZA_SHADER_UNKNOWN:
+		default:
+			return;
+		}
+	}
 
-void zaap::graphics::Renderer3D::startShader(ZA_SHADER_TYPE shader)
-{
-}
+	void Renderer3D::loadLightSetup(const LightSetup& LightSetup)
+	{
+		if (m_TextureShader)
+			m_TextureShader->loadLightSetup(&LightSetup);
+		if (m_MaterialShader)
+			m_MaterialShader->loadLightSetup(&LightSetup);
+		if (m_TerrainShader)
+			m_TerrainShader->loadLightSetup(&LightSetup);
+		//TODO all shaders still use pointers instead of the object (change that)
+	}
 
-zaap::graphics::Shader* zaap::graphics::Renderer3D::getShader(ZA_SHADER_TYPE shader)
-{
-}
+	void Renderer3D::loadScene(const Scene* scene)
+	{
+		Mat4 viewMat;
+		viewMat = scene->getCamera()->getViewMatrix();
+		
+		//view matrix
+		if (m_TextureShader)
+			m_TextureShader->setViewMatrix(viewMat);
+		if (m_MaterialShader)
+		{
+			m_MaterialShader->setViewMatrix(viewMat);
+			m_MaterialShader->setCameraPosition(scene->getCamera()->getPosition());
+		}
+		if (m_TerrainShader)
+			m_TerrainShader->setViewMatrix(viewMat);
 
-void zaap::graphics::Renderer3D::setAlphaTestingState(bool enabled)
-{
-}
+		//view frustum
+		m_ViewFrustum = scene->getCamera()->getViewFrustum();
+	}
 
-void zaap::graphics::Renderer3D::enableAlphaTesting()
-{
-}
+	void Renderer3D::startShader(ZA_SHADER_TYPE shader)
+	{
+		if (shader == m_ActiveShaderType || shader == ZA_SHADER_UNKNOWN) return;
 
-void zaap::graphics::Renderer3D::disableAlphaTesting()
-{
-}
+		Shader* cShader = getShader(m_ActiveShaderType);
+		if (cShader)
+			cShader->stop();
+
+		m_ActiveShaderType = shader;
+
+		cShader = getShader(m_ActiveShaderType);
+		if (cShader)
+			cShader->start();
+	}
+
+	Shader* Renderer3D::getShader(ZA_SHADER_TYPE shader)
+	{
+		switch (shader)
+		{
+		case ZA_SHADER_TEXTURE_SHADER:
+			return m_TextureShader;
+		case ZA_SHADER_MATERIAL_SHADER:
+			return m_MaterialShader;
+		case ZA_SHADER_TERRAIN_SHADER:
+			return m_TerrainShader;
+		case ZA_SHADER_FONT_SHADER_2D:
+			return m_FontShader2D;
+		case ZA_SHADER_UNKNOWN:
+		default:
+			return nullptr;
+		}
+	}
+
+	void Renderer3D::enableAlphaTesting()
+	{
+		setAlphaTestingState(true);
+	}
+
+	void Renderer3D::disableAlphaTesting()
+	{
+		setAlphaTestingState(false);
+	}
+
+}}
--- Zaap/src/graphics/Renderer3D.h
+++ Zaap/src/graphics/Renderer3D.h
@@ -8,20 +8,35 @@
 #include "shader/types/TextureShader.h"
 #include "shader/types/TerrainShader.h"
 #include "API/Texture2D.h"
+#include "camera/ViewFrustum.h"
 
 namespace zaap { namespace graphics {
 	class Scene;
 
-	// I left out the "API" namespace because i'm too lazy to type it every time
+	// I left out the "API" namespace because I'm too lazy to type it every time
 
+	// <class>
+	//      Renderer3D
+	//
+	// <Description>
+	//      This class is an abstract class. It could/should be passed for rendering.
+	//      
+	//      The constructor is protected. Use @Init to create a 
+	//      instance for the current API
+	//      
 	class ZAAP_API Renderer3D
 	{
 		//Static constructor
 	public:
+		// <Function>
+		//      Init
 		//
+		// <Description>
+		//      Creates a Renderer3D for the chosen API.
 		//
-		//
-		//@return: The renderer for the chousen API
+		// <Return>
+		//      The renderer for the chosen API or a nullptr in case of failure.
+		//      
 		Renderer3D* Init();
 
 	protected:
@@ -31,7 +46,10 @@ namespace zaap { namespace graphics {
 		float m_FarPlane = 1000.0f;
 		Mat4 m_ProjectionMatrix;
 
-		//Rendertarget
+		//ViewFrustum
+		ViewFrustum m_ViewFrustum; //TODO add a getter
+
+		//Render target
 		API::Texture2D* m_Rendertarget;
 		uint m_Width;
 		uint m_Height;
@@ -48,24 +66,83 @@ namespace zaap { namespace graphics {
 	public:
 		virtual ~Renderer3D() {}
 
-		//
-		// Shader stuff
-		//
+		//////////////////
+		// Shader stuff //
+		//////////////////
 
-		//Shader loaders
+		// <Function>
+		//      loadTransformationMatrix
+		//
+		// <Description>
+		//      This method loads the given TransformationMatrix into the 
+		//      currently active shader (defined by @m_ActiveShaderType).
+		// 
+		//      In case that @m_ActiveShaderType points to a shader that does
+		//      not support a TransformationMatrix it'll just return without
+		//      any actions.
+		//
+		// <Input>
+		//      A valid TransformationMatrix.
+		//
 		void loadTransformationMatrix(const Mat4& transformationMatrix) const;
-		void loadLightSetup(const LightSetup& LightSetup) const;
-		void loadScene(const Scene const* scene) const;
+		
+		// <Function>
+		//      loadLightSetup
+		//
+		// <Description>
+		//      This methods loads the given @LightSetup into all valid shaders
+		//      that support it.
+		//
+		// <Input>
+		//      A valid LightSetup.
+		//
+		void loadLightSetup(const LightSetup& LightSetup);
+		
+		// <Function>
+		//      loadScene
+		//
+		// <Description>
+		//      This methods loads and saves some components from the @Scene class.
+		//      Examples:
+		//          - @Camera position
+		//          - Current @ViewFrustum
+		//
+		// <Input>
+		//      A Instance of the @Scene.
+		//
+		void loadScene(const Scene const* scene);
 
-		//starting shaders
+		// <Function>
+		//      startShader
+		//
+		// <Description>
+		//      Starts the shader specified by the input.
+		//
+		// <Input>
+		//      @ZA_SHADER_TYPE the requested shader to start
+		//
 		void startShader(ZA_SHADER_TYPE shader);
+		
+		// <Function>
+		//      getShader
+		//
+		// <Description>
+		//      Returns the shader that is specified by the input.
+		//      This does not create changes or starts any shaders by it self.0.
+		//
+		// <Input>
+		//      @ZA_SHADER_TYPE the requested shader type
+		//
+		// <Returns>
+		//      Returns the requested shader instance or a nullptr in case of failure.
 		Shader* getShader(ZA_SHADER_TYPE shader);
 
+		// <Function>
+		//      setAlphaTestingState
 		//
-		// Alpha channel
-		//
-		virtual void setAlphaTestingState(bool enabled);
-		//these two methods just acces the "setAlphaTestingState(bool)" method
+		// <Description> //TODO add desc
+		virtual void setAlphaTestingState(bool enabled) = 0;
+		//these two methods just access the "setAlphaTestingState(bool)" method
 		inline void enableAlphaTesting();
 		inline void disableAlphaTesting();
 	};
